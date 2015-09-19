8c37551cbc71b1ecd56561cb75c5d3c41fc6c72d
Akshay Narayan <okkhoy@gmail.com>
2015-09-19 15:37:19 +0800

    completed calculating the term sentiments using running average

--- take1/term_sentiment.py
+++ take1/term_sentiment.py
@@ -24,15 +24,75 @@ allTweets = {}
 def hw():
     print 'Hello, world!'
 
+
 def lines(fp):
     print str(len(fp.readlines()))
+
+
+def cleanTweet(sentence):
+    # remove new lines in the tweet
+    #cleanedSentence = tweet.replace("\n", " ")
+    # remove urls in the tweet
+    cleanedSentence = re.sub(r"http\S+", " ", sentence.lower())
+    # remove emojis # this may not be used 'cos i m converting to ascii string anyway
+    cleanedSentence = emoji_UCS2.sub("", cleanedSentence)
+    cleanedSentence = emoji_UCS4.sub("", cleanedSentence)
+    # remove stopwords
+    cleanedSentence = englishStopwords.sub("", cleanedSentence)
+    # remove direct message to /reply to twitter handles handle
+    cleanedSentence = re.sub("@\S+", "", cleanedSentence)
+    # remove all punctuation marks
+    cleanedSentence = cleanedSentence.translate(string.maketrans("",""), punctMinusAt)
+
+    # finally return the list of words in the tweet
+    return [i for i in cleanedSentence.split() if len(i) > 1]
+
+def scoreTweetsAndTerms(fp):
+    for tweet in fp.readlines():
+        score = 0
+        jsonTweetObject = json.loads(tweet)
+        try:
+            tweetText = jsonTweetObject["text"].encode('ascii', 'ignore') # as against 'utf-8'
+            tweetText = cleanTweet(tweetText)
+            #print tweetText
+            for word in tweetText:
+                if word in scores:
+                    score = score + scores[word]
+                elif word in unscoredWords:
+                    unscoredWords[word][1] = unscoredWords[word][1] + 1
+                else:
+                    unscoredWords[word] = [0,1] # sentiment score, number of occurance
+                    #unscoredWords[word] = [0,0,0] # sentiment score, number of positive tweets, number of negative tweets
+            #print score
+            allTweets[jsonTweetObject["id_str"]] = [tweetText, score]
+            for word in tweetText:
+                if word in unscoredWords:
+                    # calculate the running average mean_n+1 = (mean_n * n + score) /2
+                    unscoredWords[word][0] = (unscoredWords[word][0] * unscoredWords[word][1] + score) / 2.0
+                    #if score > 0:
+                    #    unscoredWords[word][1] = unscoredWords[word][1] + 1
+                    #else:
+                    #    unscoredWords[word][2] = unscoredWords[word][2] + 1
+        except KeyError:
+            pass
+
+def loadafinn(fp):
+    for line in fp:
+        term, score = line.split("\t")
+        scores[term] = int(score)
+
 
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    #hw()
+    #lines(sent_file)
+    #lines(tweet_file)
+    loadafinn(sent_file)
+    scoreTweetsAndTerms(tweet_file)
+    for word in unscoredWords.keys():
+        print word,  unscoredWords[word][0]
+
 
 if __name__ == '__main__':
     main()
