f32ad96fa1f35fb142d3c652c13a46027b8daa37
Akshay Narayan <okkhoy@gmail.com>
2015-10-16 09:22:34 +0800

    removed debug statements

--- take3/multiply.py
+++ take3/multiply.py
@@ -15,7 +15,6 @@ M = 5
 N = 5
 
 def mapper(record):
-    #print "MAP", record
     # key: (i, k)
     # value: A[i,j]
     # for every k in 1..N, N is number of columns in B
@@ -24,7 +23,6 @@ def mapper(record):
     
     key = record[0]
     value = record[1:]
-    #print key, value
     
     if key == 'a':
         for k in range(N):
@@ -34,10 +32,9 @@ def mapper(record):
             mr.emit_intermediate((i, record[2]),(key, record[1], record[3])) 
 
 def reducer(key, list_of_values):
-    #print "REDUCE",
     # key: (i, k)
     # value: sum(A[i,j] * B[j,k])
-    #print key, ":", list_of_values
+
     (i,k) = key
     
     total = 0 
@@ -45,24 +42,14 @@ def reducer(key, list_of_values):
     tempB = {}
     for v in list_of_values:
         if v[0] == 'a':
-            #print "i,j:", i,  v[1], v[2]
             tempA[v[1]] = v[2]
         else:
-            #print "j,k:", v[1], k, v[2]
             tempB[v[1]] = v[2]
-    #tempA = sorted(tempA)
-    #tempB = sorted(tempB)
-    
-    #print "\n", tempA
-    #print "\n", tempB, "\n"
+
     for j in range(M):
         if j in tempA and j in tempB:
             total += tempA[j] * tempB[j]
             
-    #for a in tempA:
-    #    print "A[", i, ", ", a[0], "] = ", a[1]
-    #for b in tempB:
-    #    print "B[", b[0], ", ", k, "] = ", b[1]
     mr.emit((i,k, total))
 
 # Do not modify below this line
